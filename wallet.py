f2d79ce5aa333a25df210df12a167e94f431adb5
Daniel Kogan <54873722+daminals@users.noreply.github.com>
2021-11-11 15:55:13 -0500

    coinbase api

--- wallet.py
+++ wallet.py
@@ -1,17 +1,35 @@
 # wallet.py
-from bitcoinlib.wallets import Wallet, wallet_delete
+from bitcoinlib.wallets import Wallet, wallet_delete, wallet_create_or_open
 from bitcoinlib.mnemonic import Mnemonic
 from dotenv import load_dotenv
-import os
+import os, coinbase
+from firebase import firebase
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+
+cred = credentials.Certificate("ssfbkey.json")
 
 load_dotenv()
-key = os.environ.get('key', 3)
+CB_APIKey = os.environ.get('CB_APIKey', 3)
+CB_APISecret = os.environ.get('CB_APISecret', 3)
+FIREBASE = os.environ.get('FIREBASE', 3)
+FIREBASE_NAME = os.environ.get('FIREBASE_NAME', 3)
 
-passphrase = Mnemonic().generate()
-print(passphrase)
+# Initialize the app with a service account, granting admin privileges
+firebase_admin.initialize_app(cred, {
+    'databaseURL': FIREBASE
+})
 
-wallet = Wallet.create('dwallet', keys=passphrase)
-key1 = wallet.new_key()
-print(key1.address)
-wallet.scan()
-wallet.info()
+# As an admin, the app has access to read and write all data, regradless of Security Rules
+ref = db.reference('app/')
+print(ref.get())
+
+#passphrase = Mnemonic().generate()
+#print(passphrase)
+
+cb = coinbase.Coinbase.with_api_key(CB_APIKey, CB_APISecret)
+
+
+balance = cb.get_balance()
