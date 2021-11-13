8a914f6029c7dbb8e3f7a3d4beb54e13a06ec69c
Daniel Kogan <54873722+daminals@users.noreply.github.com>
2021-11-13 16:34:42 -0500

    finally accessing my coinbase balance

--- wallet.py
+++ wallet.py
@@ -1,19 +1,26 @@
 # wallet.py
-from bitcoinlib.wallets import Wallet, wallet_delete, wallet_create_or_open
-from bitcoinlib.mnemonic import Mnemonic
 from dotenv import load_dotenv
-import os, coinbase
+import os, coinbase, json
+from coinbase.wallet.client import Client
+from coinbase.wallet.error import AuthenticationError
 from firebase import firebase
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 
-
-cred = credentials.Certificate("ssfbkey.json")
+cred = credentials.Certificate("ssfbkey.json") #firebase key
 
 load_dotenv()
+# crypto
 CB_APIKey = os.environ.get('CB_APIKey', 3)
 CB_APISecret = os.environ.get('CB_APISecret', 3)
+
+btckey = os.environ.get('btckey', 3)
+dogekey = os.environ.get('dogekey', 3)
+ltckey = os.environ.get('ltckey', 3)
+ethkey = os.environ.get('ethkey', 3)
+
+# firebase
 FIREBASE = os.environ.get('FIREBASE', 3)
 FIREBASE_NAME = os.environ.get('FIREBASE_NAME', 3)
 
@@ -24,12 +31,23 @@ firebase_admin.initialize_app(cred, {
 
 # As an admin, the app has access to read and write all data, regradless of Security Rules
 ref = db.reference('app/')
-print(ref.get())
+#print(ref.get()) #prints whole firebase database
 
-#passphrase = Mnemonic().generate()
-#print(passphrase)
+# information
+cb = Client(CB_APIKey, CB_APISecret)
+user = cb.get_current_user()
+accounts = cb.get_accounts()
+def balance(accounts):
+    total = 0
+    message = []
+    for wallet in accounts.data:
+        message.append( str(wallet['name']) + ' ' +   str(wallet['native_balance']) )
+        value = str( wallet['native_balance']).replace('USD','')
+        total += float(value)
+    message.append( 'Total Balance: ' + 'USD ' + str(total) )
+    return('\n'.join( message ))
 
-cb = coinbase.Coinbase.with_api_key(CB_APIKey, CB_APISecret)
 
 
-balance = cb.get_balance()
+print(balance(accounts))
+#cb.get_buy_price(currency_pair = 'BTC-USD')
