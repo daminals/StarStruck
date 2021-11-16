0a94a7b4816262da69d6e4e1de4a6881fae097a1
Daniel Kogan <54873722+daminals@users.noreply.github.com>
2021-11-16 00:49:17 -0500

    working graph updates

--- static/js/reload.js
+++ static/js/reload.js
@@ -4,10 +4,14 @@ $(document).ready(function(){
       $.post("/", "update", function(){
         setTimeout(function(){
           updatesrc = $('#graph').attr('src');
-          $("#graph").attr("src", updatesrc);
+          $("#graph").removeAttr("src").attr("src", updatesrc + `?v=${getRandomInt(0,100)}`);
         }, 500);
       });
   });
 });
 
-
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
+}
