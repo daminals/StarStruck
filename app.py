e60b862d03ad2604f1f389e819b71b35877e94a6
Daniel Kogan <54873722+daminals@users.noreply.github.com>
2021-11-18 12:44:28 -0500

    :(

--- app.py
+++ app.py
@@ -20,7 +20,8 @@ def main():
         print(cb.balance()) # update balance
         os.remove("static/graph/Portfolio.png") # refresh graph
         pf.plotpf() # update graph
-    return render_template('main.html', bal=all_coin_wallets, imgsrc="static/graph/Portfolio.png")
+    bal = str.splitlines(cb.balance())
+    return render_template('main.html', bal=bal, imgsrc="static/graph/Portfolio.png")
 
 # crypto pages
 @app.route('/<coin>')
@@ -31,7 +32,7 @@ def coin_render(coin):
         abort(404)
     else:
         bal = str.splitlines(cb.balance())        
-        return render_template('main.html', bal=all_coin_wallets, imgsrc=f"static/graph/{coin}.png")
+        return render_template('main.html', bal=bal, imgsrc=f"static/graph/{coin}.png")
 
 @app.route('/coinPostRqs', methods=["GET", "POST"])
 def receive():
