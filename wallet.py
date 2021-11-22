bb9fc3e68fb7ffdc4068f9e74ccb432a406063a8
Daniel Kogan <54873722+daminals@users.noreply.github.com>
2021-11-22 18:57:05 -0500

    birthcharts

--- wallet.py
+++ wallet.py
@@ -61,7 +61,7 @@ def main():
     #cb.getUser("BTC")
     #cb.coinToCoin("BTC","DOGE",0.1)
     #cb.test_buy()
-    astro.create_chart("BTC")
+    astro.create_chart("LTC")
 
 
 if __name__ == '__main__':
